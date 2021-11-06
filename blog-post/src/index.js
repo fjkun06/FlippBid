54586101a31cfdf22d3b0d70ce799cfbb66c44a1
fjkun_06 <fjkun06@gmail.com>
2021-11-06 09:59:43 +0100

    single comment

--- blog-post/src/index.js
+++ blog-post/src/index.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import profile1 from './image/pic-1.jpg'
+import profile2 from './image/pic-2.jpg'
+import profile3 from './image/pic-3.jpg'
+import SingleComment from './SingleComment'
 
 const App = () => {
   return(
     <div className='ui comments'>
-      <div className='comment'>
-        <a href="/" className='avatar'>
-          <img alt="profile pic" />
-        </a>
-        <div className='content'>
-          <a href="/" className='author'>
-            Sarah
-          </a>
-          <div className='metadata'>
-            <span className='date'>
-              Today at 5:00PM
-            </span>
-          </div>
-          <div className='Text'>
-            It's amazing
-          </div>
-        </div>
-      </div>
+      <SingleComment 
+      name='Alex' 
+      date='Todat at 5:00PM'
+      text = 'it is cool'
+      pixture = {profile1}/>
+      <SingleComment 
+      name='Jack' 
+      date='Todat at 5:00PM'
+      text='excellent job'
+      pixture = {profile2}
+      />
+      <SingleComment 
+      name='Sarah' 
+      date='Todat at 6:00PM'
+      text='no kidding...'
+      pixture = {profile3}
+      />
+
     </div>
   );
 }
