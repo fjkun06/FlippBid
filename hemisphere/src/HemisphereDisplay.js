9dca83ed9dc56b448cf8152b116882e1c45c9e8d
fjkun_06 <fjkun06@gmail.com>
2021-11-06 20:34:03 +0100

    hemisphere2

---
+++ hemisphere/src/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HemisphereDisplay = () => {
+    return(
+        <div>
+            Wtf you think yu doing??
+        </div>
+    )
+}
+
+export default HemisphereDisplay;
