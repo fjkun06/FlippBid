302399c329650edde7892d3d91dc1f8c0d3fc750
= <=>
2023-09-19 01:10:39 +0100

    refactored

--- src/screens/home/Header.tsx
+++ src/screens/home/Header.tsx
@@ -2,6 +2,7 @@ import { HeaderCard, StoreButton, Text } from "components";
 import { stores } from "components/footer/data";
 import Image from "next/image";
 import React from "react";
+import { appDesc, headerCards } from "./data";
 
 const Header = () => {
   return (
@@ -30,29 +31,14 @@ const Header = () => {
               quality={100}
               alt="man-sorrounded-by-orbits"
             />
-            <HeaderCard
-              className="-right-[26px] sm:right-[20px] lg-right-[33px] bottom-1 sm:bottom-[33px] !w-[147px] sm:!w-[225px] !h-[76px] sm:!h-[103px]"
-              title="Find My Deal"
-              description="Get Full Property, Data, Comps, ARV's, Ownership Info, Foreclosure Status and MLS Listing within seconds!"
-              borderColor="grey"
-            />
-            <HeaderCard
-              className="-right-[31px] sm:right-[14px] top-[70px] lg:-right-[82px] sm:top-[160px] !w-[138px] !h-[76px] sm:!w-[204px] sm:!h-[100px]"
-              title="Sell My Deal"
-              description="Showcase your property to our Community of Buyers, with Full Property Data, Pictures, Video Walkthroughs."
-              borderColor="secondary"
-            />
-            <HeaderCard
-              className="lg:!gap-2 -left-8 sm:left-[35px] lg:-left-[95px] bottom-1 sm:bottom-[33px] lg:bottom-[93px]"
-              title="Verified Deals"
-              description="Get Live Email, Text, App Notifications DIRECT from the Contract Holders. No Brokers, No Middlemen, No Spam!"
-              borderColor="gradient"
-            />
+            {headerCards.map((props, i) => (
+              <HeaderCard key={i} {...props} />
+            ))}
           </div>
         </div>
       </div>
-      <div className="h-[270px] s1400:h-[200px] bg-red-2000 flex px-[105px] justify-center items-center w-full">
-        <div className="h-full s1400:h-max w-screen sm:w-full flex items-center justify-between max-w-[1300px] bg-greenn-500 flex-col s1400:flex-row ">
+      <div className="h-[270px] s1400:h-[200px] flex px-[105px] justify-center items-center w-full">
+        <div className="h-full s1400:h-max w-screen sm:w-full flex items-center justify-between max-w-[1300px] flex-col s1400:flex-row ">
           <div className="flex px-2 sm:px-0  gap-2.5 w-[415px] justify-between mt-10 s1400:mt-0">
             {stores.map((props) => (
               <StoreButton
@@ -63,14 +49,9 @@ const Header = () => {
             ))}
           </div>
           <div className="max-w-full px-2 sm:px-0  sm:max-w-[470px] s1400:max-w-[627px] h-max sm:h-[100px]  mb-5 sm:mb-10 s1400:mb-0 s1400:h-max flex flex-col gap-5 s1400:gap-1">
-            <Text
-              text="FlippBidd is a Real Estate Investment Application where Investors, Wholesalers, Brokers, Acquisition and Disposition specialists can Locate, Showcase and Discuss their Investment Deals DIRECT!"
-              className="leading-6 s1400:leading-[18.22px] text-[13px] sm:text-[15px] text-[#455A64] h-max"
-            />
-            <Text
-              text="Free to Download and Use"
-              className="leading-[18.22px] font-semibold text-[17px] text-orange cursor-pointer"
-            />
+            {appDesc.map((props, i) => (
+              <Text key={i} {...props} />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/screens/home/data.tsx
@@ -0,0 +1,40 @@
+export const headerCards = [
+  {
+    className:
+      "-right-[26px] sm:right-[20px] lg-right-[33px] bottom-1 sm:bottom-[33px] !w-[147px] sm:!w-[225px] !h-[76px] sm:!h-[103px]",
+    title: "Find My Deal",
+    description:
+      "Get Full Property, Data, Comps, ARV's, Ownership Info, Foreclosure Status and MLS Listing within seconds!",
+    borderColor: "grey" as any,
+  },
+  {
+    className:
+      "-right-[31px] sm:right-[14px] top-[70px] lg:-right-[82px] sm:top-[160px] !w-[138px] !h-[76px] sm:!w-[204px] sm:!h-[100px]",
+    title: "Sell My Deal",
+    description:
+      "Showcase your property to our Community of Buyers, with Full Property Data, Pictures, Video Walkthroughs.",
+    borderColor: "secondary" as any,
+  },
+  {
+    className:
+      "lg:!gap-2 -left-8 sm:left-[35px] lg:-left-[95px] bottom-1 sm:bottom-[33px] lg:bottom-[93px]",
+    title: "Verified Deals",
+    description:
+      "Get Live Email, Text, App Notifications DIRECT from the Contract Holders. No Brokers, No Middlemen, No Spam!",
+    borderColor: "gradient" as any,
+  },
+];
+
+
+export const appDesc = [
+  {
+    text: "FlippBidd is a Real Estate Investment Application where Investors, Wholesalers, Brokers, Acquisition and Disposition specialists can Locate, Showcase and Discuss their Investment Deals DIRECT!",
+    className:
+      "leading-6 s1400:leading-[18.22px] text-[13px] sm:text-[15px] text-[#455A64] h-max",
+  },
+  {
+    text: "Free to Download and Use",
+    className:
+      "leading-[18.22px] font-semibold text-[17px] text-orange cursor-pointer",
+  },
+];
